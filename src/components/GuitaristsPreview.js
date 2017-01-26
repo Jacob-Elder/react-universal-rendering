94e0b69b5e9d077ff230c5cb0b690f2e10424778
Jacob Elder <jacobelder@Jacobs-MacBook-Pro.local>
2017-01-25 21:12:00 -0800

    included bootstrap and applied grid to images on the index page

--- src/components/GuitaristsPreview.js
+++ src/components/GuitaristsPreview.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 export default class GuitaristsPreview extends React.Component {
 	render() {
 		return (
-				<div className="guitarist-preview">
+				<div className="guitarist-preview col-sm-6">
 				<Link to={`/guitarist/${this.props.id}`}>
 					<img className="profilePic" src={this.props.image}/>
 				</Link>
--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -6,8 +6,12 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <div className="home">
-        <div className="guitarists-selector">
-          {guitarists.map(guitarist => <GuitaristsPreview key={guitarist.id} {...guitarist} />)}
+        <div className="container">
+          <div className="row">
+            <div className="guitarists-selector">
+              {guitarists.map(guitarist => <GuitaristsPreview key={guitarist.id} {...guitarist} />)}
+            </div>
+          </div>
         </div>
       </div>
     );
